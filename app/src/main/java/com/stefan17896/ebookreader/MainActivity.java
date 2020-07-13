92dfcdb8cd6fcee00d9a93285ac791ae95cfc5b2
stefan17896 <46993275+stefan17896@users.noreply.github.com>
2020-07-13 18:15:25 +0200

    working epub reading

--- app/src/main/java/com/stefan17896/ebookreader/MainActivity.java
+++ app/src/main/java/com/stefan17896/ebookreader/MainActivity.java
@@ -2,13 +2,38 @@ package com.stefan17896.ebookreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.folioreader.Constants;
+import com.folioreader.FolioReader;
+import com.folioreader.Config;
 
-public class MainActivity extends AppCompatActivity {
+import android.R.layout;
+import android.R.raw;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+
+public class
+MainActivity extends AppCompatActivity {
+    FolioReader folioReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        folioReader = FolioReader.get();
+        //folioReader.openBook(R.raw.book1);
+        //folioReader.openBook("file:///android_asset/book1.epub");
+    }
+    public void test(View view){
+        //folioReader.openBook(R.raw.book1);
+        test2(view);
+    }
+    public void test2(View view){
+        //folioReader.openBook("file:///android_asset/book1.epub");
+        folioReader.openBook(R.raw.book1);
     }
 }
